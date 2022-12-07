0b9612f38d7115b708f18bd9929944ff54d639f3
Rasmus <49008491+Eklund89@users.noreply.github.com>
2022-12-07 15:25:03 +0100

    Did 1,2,3,4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let out = '';
+    if (num < 0) {
+        out = "ERROR"
+    } else {
+        for (let i = 0; i < num; i++) {
+            out += string;
+        }
+    }
+    return out;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, remove) {
+    let args = Array.from(arguments).slice(1)
+    // args = args.slice(1)
 
+    for (const i of args) {
+        if (arr.includes(i)){
+            arr.splice(arr.indexOf(i), 1)
+        }
+    }
+
+    return arr
 };
 
 // Do not edit below this line
