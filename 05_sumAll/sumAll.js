a4db4b633d0a5b14a6532a405d12721344b346ac
Rasmus <49008491+Eklund89@users.noreply.github.com>
2022-12-07 21:32:33 +0100

    Did 5 and 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    let sum = 0;
+    let start, stop;
+    if (typeof a !== 'number' || typeof b !== 'number' || a<0 || b<0) {
+        return 'ERROR'
+    }
+    start = (a < b) ? a : b;
+    stop = (a > b) ? a : b;
+    for (let i = start; i < stop + 1; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
